b2e91a87e0de4989ab87f7473591b17b965509b9
Faiyaz-F <faiyazshaikh@eng.rizvi.edu.in>
2024-11-21 20:54:47 +0530

    Add files via upload

---
+++ Experiment 1/exp1 task1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    printf("Full name\t :Sanjay singh \n");
+    printf("Age\t\t :26\n");
+    printf("class\t\t :Fe\n");
+    printf("Division\t :D\n");
+    printf("UIN\t\t :241P005\n");
+
+     return 0;
+}
